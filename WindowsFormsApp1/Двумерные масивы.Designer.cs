3217a3cb4dfc022bbb256b736170e6776ac7641a
PC <PC@DESKTOP-EQ2CG60>
2024-04-18 21:44:42 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Двумерные масивы.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmMass
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtm = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRez = new System.Windows.Forms.TextBox();
+            this.dvgMass = new System.Windows.Forms.DataGridView();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgMass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество строк n=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(22, 65);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(196, 26);
+            this.txtn.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(18, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество столбцов  m=";
+            // 
+            // txtm
+            // 
+            this.txtm.Location = new System.Drawing.Point(22, 206);
+            this.txtm.Name = "txtm";
+            this.txtm.Size = new System.Drawing.Size(196, 26);
+            this.txtm.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(18, 312);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Результат:";
+            // 
+            // txtRez
+            // 
+            this.txtRez.Location = new System.Drawing.Point(22, 339);
+            this.txtRez.Multiline = true;
+            this.txtRez.Name = "txtRez";
+            this.txtRez.Size = new System.Drawing.Size(297, 192);
+            this.txtRez.TabIndex = 5;
+            // 
+            // dvgMass
+            // 
+            this.dvgMass.AllowUserToOrderColumns = true;
+            this.dvgMass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgMass.Location = new System.Drawing.Point(393, 38);
+            this.dvgMass.Name = "dvgMass";
+            this.dvgMass.Size = new System.Drawing.Size(771, 493);
+            this.dvgMass.TabIndex = 6;
+            this.dvgMass.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Location = new System.Drawing.Point(22, 587);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(297, 47);
+            this.cmdStart.TabIndex = 7;
+            this.cmdStart.Text = "Вычислить";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(393, 587);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(297, 47);
+            this.cmdClear.TabIndex = 8;
+            this.cmdClear.Text = "Очистить";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(867, 587);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(297, 47);
+            this.cmdExit.TabIndex = 9;
+            this.cmdExit.Text = "Завершить работу";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // frmMass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.dvgMass);
+            this.Controls.Add(this.txtRez);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtn);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMass";
+            this.Text = "Двумерные масивы";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgMass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRez;
+        private System.Windows.Forms.DataGridView dvgMass;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Button cmdExit;
+    }
+}
+
---
+++ WindowsFormsApp1/Двумерные масивы.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMass : Form
+    {
+        
+
+        public frmMass()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            int n, m, nn = 0;
+            float summ = 0, sr = 0;
+            n = Int32.Parse(txtn.Text);
+            m = Int32.Parse(txtm.Text);
+            int[,] A = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j =  0; j <= m - 1; j++)
+                {
+                    A[i, j] = rnd.Next(-50, 50);
+                }
+            }
+            dvgMass.RowCount = n;
+            dvgMass.ColumnCount = m;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= m - 1; j++)
+                {
+                    dvgMass.Rows[i].Cells[j].Value = A[i, j];
+                }
+            }
+            for (int j = 0; j <= m - 1; j++) dvgMass.Columns[j].Width = 50;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= m - 1; j++)
+                {
+                   if (A[i,j] > 0 )
+                    {
+                        summ = summ + A[i, j];
+                        nn += 1;
+                    }
+                }
+            }
+            sr = summ / nn;
+            txtRez.Text = "Среднее арифметическое = " + Math.Round(sr, 5).ToString() + Environment.NewLine + "Количество положительных элементов = " + nn.ToString();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            txtm.Text = "";
+            txtn.Text = "";
+            txtRez.Text = "";
+            dvgMass.Rows.Clear();
+            dvgMass.Columns.Clear();
+        }
+    }
+}
